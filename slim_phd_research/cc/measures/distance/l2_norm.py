fc504a3d1f9416b3f3af255675aa01bcb9b5fbf8
Henok Ghebrechristos <hghebrechristos@BII.local>
2019-01-23 10:56:02 -0700

    l1 and l2 norm measures

--- slim_phd_research/cc/measures/distance/l2_norm.py
+++ slim_phd_research/cc/measures/distance/l2_norm.py
@@ -1,16 +1,15 @@
+from numpy.linalg import norm
 import tensorflow as tf
 
-def SSIM(patch_1, patch_2):
+
+def L2Norm(patch_1, patch_2):
     assert patch_1.shape == patch_2.shape, "Patches must have similar tensor shapes of [p_w, p_h, c]"
     assert patch_1 != patch_2, "Patches are binary equivalent, Distance = 0"
 
     sess = tf.get_default_session()
 
-    sess = tf.get_default_session()
+    # combine the two tensors into one
+    patch_data = sess.run(
+        tf.concat([patch_1, patch_2], 0))
 
-    # flatten the tensor into a sigle dimensinoal array
-    patch_1 = sess.run(tf.reshape(patch_1, [-1]))
-    patch_2 = sess.run(tf.reshape(patch_2, [-1]))
-
-    
-    return ce
+    return round(norm(patch_data, 2),2)
--- slim_phd_research/cc/measures/distance/max_norm.py
+++ slim_phd_research/cc/measures/distance/max_norm.py
@@ -1,16 +1,16 @@
+from numpy.linalg import norm
+from numpy import inf
 import tensorflow as tf
 
-def SSIM(patch_1, patch_2):
+
+def MaxNorm(patch_1, patch_2):
     assert patch_1.shape == patch_2.shape, "Patches must have similar tensor shapes of [p_w, p_h, c]"
     assert patch_1 != patch_2, "Patches are binary equivalent, Distance = 0"
 
     sess = tf.get_default_session()
 
-    sess = tf.get_default_session()
+    # combine the two tensors into one
+    patch_data = sess.run(
+        tf.concat([patch_1, patch_2], 0))
 
-    # flatten the tensor into a sigle dimensinoal array
-    patch_1 = sess.run(tf.reshape(patch_1, [-1]))
-    patch_2 = sess.run(tf.reshape(patch_2, [-1]))
-
-    
-    return ce
+    return round(norm(patch_data, inf))
