e01c2534f078d50918179027a7c074b50449b112
Henok Ghebrechristos <hghebrechristos@BII.local>
2019-02-05 16:07:13 -0700

    uptodate

---
+++ phd_research/cc/layers/cc_layer.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+from cc.measures.distance import *
+from cc.measures.standalone import *
+
+TFKLayer = tf.keras.layers.Layer
+
+
+class CCLayer(TFKLayer):
+
+    def __init__(self, num_outputs):
+        self.num_outputs = num_outputs
