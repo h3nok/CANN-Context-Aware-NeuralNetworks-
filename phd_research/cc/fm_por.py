0652ab8f917dcb331435a99adb96ab9a42737d81
Henok Ghebrechristos <hghebrechristos@BII.local>
2019-02-05 17:19:58 -0700

    21

--- phd_research/cc/fm_por.py
+++ phd_research/cc/fm_por.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 import numpy as np
 from patch_proposals import generate_patches_v2
-from map_measure import Measure, MeasureType
+from map_measure import Measure, MeasureType, Ordering
 from reconstructor import reconstruct_from_patches
 from PIL import Image
 tf_func = tf.py_func
@@ -9,35 +9,44 @@ tf_contrib = tf.contrib
 
 
 class FM_POR(object):
-    input = None
-    input_size = None
-    patch_size = None
-    patches = None
-    measure = None
+    Input = None
+    InputSize = None
+    PatchSize = None
+    Patches = None
+    Ranker = None
+    Output = None
+    RankOrder = None
 
-    def __init__(self, name, input_feature_map, input_size, patch_size, measure=Measure.MI):
-        assert tf_contrib.framework.is_tensor(input_feature_map), "Input must be instance of tf.Tensor"
+    def __init__(self, name, input_feature_map, input_size, patch_size, measure=Measure.MI, order=Ordering.Ascending):
+        assert tf_contrib.framework.is_tensor(
+            input_feature_map), "Input must be instance of tf.Tensor"
         assert isinstance(
             measure, Measure), "Supplied measure doesn't exist, measure: {}".format(measure.value)
         assert input_size > patch_size, "Input size must be orders of magnitude larger than patch size"
         assert input_size != 0, "Input shape must be > 0"
         assert patch_size != 0, "Patch shape must be > 0"
 
-        self.input = input_feature_map
-        self.input_size = input_size
-        self.patch_size = patch_size
+        self.Input = input_feature_map
+        self.InputSize = input_size
+        self.PatchSize = patch_size
+        self.Ranker = measure
 
     def Apply(self):
-        self.patches = generate_patches_v2(self.input, self.input_size,
-                                           self.input_size, self.patch_size, self.patch_size)
+        self.Patches = generate_patches_v2(self.Input, self.InputSize,
+                                           self.InputSize, self.PatchSize, self.PatchSize)
 
-        print (self.patches)
+        self.Output = reconstruct_from_patches(
+            self.Patches, self.InputSize, self.InputSize, self.Ranker)
+        print(self.Patches)
+
 
 def test():
-    slice56 = np.random.random((32, 32,3))
-    formatted = tf.convert_to_tensor((slice56 * 255 / np.max(slice56)).astype('uint8'))
+    slice56 = np.random.random((32, 32, 3))
+    formatted = tf.convert_to_tensor(
+        (slice56 * 255 / np.max(slice56)).astype('uint8'))
     fm_por_layer_1 = FM_POR('fm_por_1', formatted, 32, 8)
     fm_por_layer_1.Apply()
+
     # img = Image.fromarray(formatted)
     # img.show()
 
