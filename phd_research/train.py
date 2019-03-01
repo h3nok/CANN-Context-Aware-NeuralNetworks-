d9d0654456aec93ee0bfbbf22c1909d6cae664a7
h3nok <nkenok@outlook.com>
2019-03-01 10:41:08 -0700

    wire up curriculum learning

--- phd_research/train.py
+++ phd_research/train.py
@@ -496,6 +496,10 @@ def main(_):
                 capacity=5 * FLAGS.batch_size)
             labels = slim.one_hot_encoding(
                 labels, dataset.num_classes - FLAGS.labels_offset)
+            
+            print(type(images))
+            print(images)
+            rewerw
             # TODO- insert curriculum learning here
             batch_queue = slim.prefetch_queue.prefetch_queue(
                 [images, labels], capacity=2 * deploy_config.num_clones)
