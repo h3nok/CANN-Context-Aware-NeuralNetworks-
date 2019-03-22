d80dd754a35652f9dfddf1bcc454f2076a43305a
h3nok <nkenok@outlook.com>
2019-03-22 15:55:37 -0600

    'plotter'

--- research/tfsummary.py
+++ research/tfsummary.py
@@ -3,6 +3,7 @@ from enum import Enum
 from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
 import matplotlib.pyplot as plt
 import os
+import numpy as np
 
 class InceptionV2Loss(Enum):
 	regularization = "regularization_loss_1"
@@ -13,14 +14,10 @@ class InceptionV2Loss(Enum):
 class Summary(object):
 	event_file = ""
 	tags = []
-	wall_time = []
-	steps = []
-	total_loss = []
 	reg_loss = []
 	softmax_loss = []
 
-	def __init__(self, event_file_path, network="", measure="",
-					dataset="", iterations="", training=True):
+	def __init__(self, event_file_path, training=True):
 		assert event_file_path is not None, "Must supply a valid events file\n"
 		self.event_file = event_file_path
 
@@ -33,44 +30,44 @@ class Summary(object):
 		regularization_loss_summary = event_acc.Scalars(InceptionV2Loss.regularization.value)
 		softmax_loss_summary = event_acc.Scalars(InceptionV2Loss.softmax.value)
 
+		wall_time = []
+		steps = []
+		total_loss = []
+		reg_loss = []
+		softmax_loss = []
 		for item in total_loss_summary:
-			self.wall_time.append(item[0])
-			self.steps.append(item[1])
-			self.total_loss.append(item[2])
+			wall_time.append(item[0])
+			steps.append(item[1])
+			total_loss.append(item[2])
 
 		for item in regularization_loss_summary:
-			self.reg_loss.append(item[2])
+			reg_loss.append(item[2])
 
 		for item in softmax_loss_summary:
-			self.softmax_loss.append(item[2])
+			softmax_loss.append(item[2])
 
-	def plot_all_losses(self):
-		plt.plot(self.steps, self.softmax_loss, label="Cross-Entropy Loss")
-		plt.plot(self.steps, self.reg_loss, label="Regularization Loss")
-		plt.plot(self.steps, self.total_loss, label="Total Loss")
+		return steps, total_loss, reg_loss, softmax_loss
 
-		plt.xlabel("Training Steps")
-		plt.ylabel("Loss")
-		plt.title("Training Loss")
-		plt.legend(loc='upper right', frameon=True)
-		plt.show()
 
-	def plot_total_loss(self, metric):
-		plt.plot(self.steps, self.total_loss, label="Total Loss")
+def plot_multiple(data, plot='Softmax loss', network='Inception V2'):
+	for run in data:
+		plt.plot(run['Steps'], run[plot], marker='o',
+									label=run['Metric'], c=np.random.rand(3))
 
-		plt.xlabel("Training Steps")
-		plt.ylabel("Loss")
-		plt.title("Training Loss, Metric: {}".format(metric))
-		plt.legend(loc='upper right', frameon=True)
-		plt.show()
-		plt.close()
-		self.total_loss.clear()
-		self.steps.clear()
+	plt.xlabel("Training Steps")
+	plt.ylabel("Loss")
+	plt.title("{} {}".format(network, plot))
+	plt.legend(loc='upper right', frameon=True)
+	plt.savefig('{}_{}.svg'.format(plot,network), format='svg', dpi=1000)
+	plt.show()
+	plt.close()
 
 
 if __name__ == '__main__':
 	summaries_dir = "E:\\Thesis\\CC_V2\\summaries"
 	iterations = 10000
+	training_data = []
+	data = {}
 	for measure in os.listdir(summaries_dir):
 		metric = measure
 		event_file_dir = os.path.join(summaries_dir, metric, str(iterations))
@@ -82,7 +79,14 @@ if __name__ == '__main__':
 		if not event_file: continue
 		log_file = os.path.join(event_file_dir, event_file)
 		s = Summary(log_file)
-		s.process()
-		s.plot_total_loss(metric)
+		steps, loss, reg_loss, softmax_loss = s.process()
+		data = {
+			"Steps": steps,
+			"Total loss": loss,
+			"Metric": metric,
+			"Reg loss": reg_loss,
+			"Softmax loss": softmax_loss
+		}
+		training_data.append(data)
 
-
+	plot_multiple(training_data)
