38d81ee97c2a657a032a32ebcc2369b76980a20d
Henok <nkenok@outlook.com>
2019-04-02 11:04:36 -0600

    uptodate

--- research/eval.sh
+++ research/eval.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-python3 evaluate.py --model_name inception_v2 --dataset_name cifar10 --iter 10000 --metric je --training_mode curriculum
+python3 evaluate.py --model_name inception_v1 --dataset_name cifar10 --iter 10000 
 
