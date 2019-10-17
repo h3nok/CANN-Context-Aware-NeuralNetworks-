f2f72e8a18011079abe0e55c6639fa18b196ecc9
h3nok <nkenok@outlook.com>
2019-10-17 10:56:52 -0600

    train config

--- research/config/configurations.py
+++ research/config/configurations.py
@@ -81,3 +81,4 @@ class TrainingFlags(_base):
 
         return obj
 
+
--- research/train_tf.py
+++ research/train_tf.py
@@ -4,7 +4,7 @@ import database_interface as dbi
 
 
 sqlsess = dbi.SQLSession(host='hgh-d-22')
-config = TrainingConfig(configfile="E:\\viNet_RnD\\Training\\Config\\viNet_2.2_E3_v2.yaml",
+config = TrainingConfig(configfile="E:\\viNet_RnD\\Training\\Config\\cifar10_train_config.yaml",
                         sqlsession=sqlsess)
 t_params = config.params
 
