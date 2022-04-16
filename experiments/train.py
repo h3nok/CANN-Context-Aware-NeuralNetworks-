3fed8b5d6e13a10ee38ad5ae69d1bbf8491a2548
Henok <nkenok@gmail.com>
2022-04-16 10:39:59 -0500

    Moving CLO to tf2

--- experiments/train.py
+++ experiments/train.py
@@ -24,6 +24,7 @@ if __name__ == "__main__":
     parser.add_argument('--config_file',
                         type=str,
                         default='../configurations/deepclo_train_config.ini',
+                        required=True,
                         help="Path to .ini config file ")
 
     args = parser.parse_args()
